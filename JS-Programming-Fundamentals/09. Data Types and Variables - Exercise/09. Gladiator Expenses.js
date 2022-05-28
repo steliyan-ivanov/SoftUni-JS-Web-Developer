5f5e75bdae8d9f9b51b2ef7384a52b4bae67117e
Steliyan Ivanov <hello@steliyan.com>
2022-05-28 15:15:42 +0300

    commit

--- JS-Programming-Fundamentals/09. Data Types and Variables - Exercise/09. Gladiator Expenses.js
+++ JS-Programming-Fundamentals/09. Data Types and Variables - Exercise/09. Gladiator Expenses.js
@@ -1,3 +1,38 @@
-function gladiator(params) {
+function gladiator(lostFights, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+
+    let counter = 0;
+    let helmetBroken = 0;
+    let swordBroken = 0;
+    let shieldBroken = 0;
+    let armorBroken = 0;
+
+    for (let i = 1; i <= lostFights; i++) {
+        counter += 1;
+
+        if (counter % 2 == 0) {
+            helmetBroken += 1;
+        }
+        if (counter % 3 == 0) {
+            swordBroken += 1;
+        }
+        if (counter % 6 == 0) {
+            shieldBroken += 1;
+        }
+        if (counter % 12 == 0) {
+            armorBroken += 1;
+        }
+    }
+
+    let repairPrice = (helmetBroken * helmetPrice) + (swordBroken * swordPrice) + (shieldBroken * shieldPrice) + (armorBroken * armorPrice);
+    console.log(`Gladiator expenses: ${repairPrice.toFixed(2)} aureus`);
+
 
 }
+
+gladiator(23,
+    12.50,
+    21.50,
+    40,
+    200
+
+)
