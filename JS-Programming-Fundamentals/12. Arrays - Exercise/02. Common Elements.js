17eac00fce607b7777aea6109a001817c0e408dd
Steliyan Ivanov <hello@steliyan.com>
2022-06-15 16:42:10 +0300

    commit

--- JS-Programming-Fundamentals/12. Arrays - Exercise/02. Common Elements.js
+++ JS-Programming-Fundamentals/12. Arrays - Exercise/02. Common Elements.js
@@ -1,18 +1,15 @@
 function commonElements(array1, array2) {
 
-    let matches = [];
+    let matches = '';
 
     for (let i = 0; i < array1.length; i++) {
-
-        if (array1[i] == array2[i]) {
-            console.log(array1[i]);
+        for (let r = 0; r < array2.length; r++) {
+            if (array1[i] === array2[r]) {
+                matches = array2[r]
+                console.log(matches);
+            }
         }
-
     }
-
-
-
-
 }
 
 commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2'])
---
+++ JS-Programming-Fundamentals/12. Arrays - Exercise/03. Merge Arrays.js
@@ -0,0 +1,29 @@
+function mergeArrays(string1, string2) {
+
+    let array1 = [];
+    let array2 = [];
+    let array3 = [];
+
+    for (let el of string1) {
+        array1.push(Number(el))
+    }
+
+    for (let el of string2) {
+        array2.push(Number(el))
+    }
+
+
+    for (let i = 0; i < array1.length; i++) {
+
+        if (i % 2 == 0) {
+            array3.push(array1[i] + array2[i])
+        } else {
+            array3.push(`${array1[i]}${array2[i]}`)
+        }
+    }
+
+    console.log(array3.join(" - "));
+
+}
+
+mergeArrays(['5', '15', '23', '56', '35'], ['17', '22', '87', '36', '11'])
