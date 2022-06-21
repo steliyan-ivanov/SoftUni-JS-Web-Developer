eda85f4e27cf1337df164071b550fba95ef978e0
Steliyan Ivanov <hello@steliyan.com>
2022-06-21 23:28:18 +0300

    commit

--- JS-Programming-Fundamentals/12. Arrays - Exercise/08. Magic Sum.js
+++ JS-Programming-Fundamentals/12. Arrays - Exercise/08. Magic Sum.js
@@ -1,9 +1,19 @@
 function magicSum(array, number) {
 
-    let
 
+    for (let i = 0; i < array.length; i++) {
 
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] == number) {
+                console.log(`${array[i]} ${array[j]}`);
+            }
+
+        }
+
+    }
 
 }
 
-magicSum([1, 7, 6, 2, 19, 23], 8)
+magicSum([14, 20, 60, 13, 7, 19, 8],
+    27
+)
