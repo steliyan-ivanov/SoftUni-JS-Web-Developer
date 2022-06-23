8151d3181153222840e813cee232e518197dd75e
Steliyan Ivanov <hello@steliyan.com>
2022-06-23 14:43:38 +0300

    commit

--- JS-Programming-Fundamentals/12. Arrays - Exercise/more exercises/04. Non-Decreasing Subset.js
+++ JS-Programming-Fundamentals/12. Arrays - Exercise/more exercises/04. Non-Decreasing Subset.js
@@ -1,7 +1,18 @@
 function solve(array) {
 
+    let currentBiggest = array[0];
+    let newArray = [];
+    newArray.push(currentBiggest);
 
+    for (let i = 1; i < array.length; i++) {
 
+        if (array[i] >= currentBiggest) {
+            currentBiggest = array[i];
+            newArray.push(currentBiggest)
+        }
+
+    }
+    console.log(newArray.join(" "));
 }
 
-solve([1, 2, 3, 4])
+solve([1, 3, 8, 4, 10, 12, 3, 2, 24])
