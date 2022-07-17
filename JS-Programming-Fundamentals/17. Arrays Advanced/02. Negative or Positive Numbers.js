2aac4dc087f2f9889864109fd1bcad09062f9778
Steliyan Ivanov <hello@steliyan.com>
2022-07-17 20:01:01 +0300

    commit

---
+++ JS-Programming-Fundamentals/17. Arrays Advanced/02. Negative or Positive Numbers.js
@@ -0,0 +1,17 @@
+function solve(arr) {
+
+    let result = [];
+
+    for (let el of arr) {
+
+        if (el >= 0) {
+            result.push(el)
+        } else {
+            result.unshift(el);
+        }
+    }
+
+    console.log(result.join('\n'));
+}
+
+solve(['7', '-2', '8', '9'])
