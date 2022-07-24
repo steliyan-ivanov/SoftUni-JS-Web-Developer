3396ad9ab94753ffb8420ce64399446ea2f10730
Steliyan Ivanov <hello@steliyan.com>
2022-07-24 19:41:43 +0300

    commit

--- JS-Programming-Fundamentals/17. Arrays Advanced - Lab/08. Array Manipulations.js
+++ JS-Programming-Fundamentals/17. Arrays Advanced - Lab/08. Array Manipulations.js
@@ -1,8 +1,30 @@
 function solve(commands) {
 
     let arr = commands.shift().split(' ').map(Number);
-    console.log(arr);
 
+    for (let i = 0; i < commands.length; i++) {
+        let [command, firstNum, secondNum] = commands[i].split(' ');
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
+
+        switch (command) {
+            case "Add":
+                arr.push(firstNum);
+                break;
+            case "Remove":
+                arr.filter(el => el !== firstNum);
+                console.log(arr);
+                break;
+            case "RemoveAt":
+
+                break;
+            case "Insert":
+
+                break;
+        }
+
+    }
+    console.log(arr);
 }
 
 solve(['4 19 2 53 6 43',
