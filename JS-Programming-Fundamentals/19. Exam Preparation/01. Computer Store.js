4381746717c3e32afb4475464cafe9530bc272ed
Steliyan Ivanov <hello@steliyan.com>
2022-07-26 23:11:08 +0300

    commit

--- JS-Programming-Fundamentals/19. Exam Preparation/01. Computer Store.js
+++ JS-Programming-Fundamentals/19. Exam Preparation/01. Computer Store.js
@@ -1,5 +1,36 @@
 function computerStore(arr) {
-
+    let i = 0;
+    let partPrice = 0;
+    let priceWithoutTaxes = 0;
+    let taxes = 0;
+    let customerIsRegular = true;
+    while (arr[i] !== "special" && arr[i] !== "regular") {
+        if (arr[i] < 0) {
+            console.log("Invalid price!");
+            i++;
+            continue;
+        }
+        partPrice = Number(arr[i]);
+        priceWithoutTaxes += partPrice;
+        taxes += partPrice * 0.2;
+        i++;
+    }
+    let totalPrice = taxes + priceWithoutTaxes;
+    if (arr[i] === "special") {
+        customerIsRegular = false;
+    }
+    if (!customerIsRegular) {
+        totalPrice *= 0.9;
+    }
+    if (totalPrice === 0) {
+        console.log("Invalid order!");
+        return;
+    }
+    console.log("Congratulations you've just bought a new computer!");
+    console.log(`Price without taxes: ${priceWithoutTaxes.toFixed(2)}$`);
+    console.log(`Taxes: ${taxes.toFixed(2)}$`);
+    console.log("-----------");
+    console.log(`Total price: ${totalPrice.toFixed(2)}$`);
 }
 
 computerStore([
